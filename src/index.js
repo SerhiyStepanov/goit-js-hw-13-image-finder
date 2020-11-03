56ba81557b92a0d73da02da28ccd18f25588f92f
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-03 15:15:11 +0200

    add template and icons

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-fetch('https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=8315600-a916a243d8ea2edafddc43bfd')
-    .then(res => res.json()).then(date => console.log(date))
+import templateCards from './template-cards.hbs'
+
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal'
+
+const inputEl = document.querySelector('INPUT') 
+const cardContainer = document.querySelector('.gallery')
+
+fetchCard().then(renderCard)
+
+function fetchCard() {
+        return fetch(`${BASE_URL}&q=dog&page=1&per_page=12&key=8315600-a916a243d8ea2edafddc43bfd`)
+        .then(res => {return res.json()})
+}
+
+function renderCard(cards) {
+    const markup = templateCards(cards.hits)
+    cardContainer.innerHTML = markup
+}
