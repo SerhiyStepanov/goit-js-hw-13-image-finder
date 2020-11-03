9ad35e58b8d26cd961e15141f1e69a0564286158
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-03 20:03:43 +0200

    add error

--- src/index.js
+++ src/index.js
@@ -2,27 +2,33 @@ import templateCards from './template-cards.hbs'
 
 const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal'
 
-const inputEl = document.querySelector('INPUT') 
+const form = document.querySelector('#search-form')
+const searchInputEl = document.querySelector('INPUT') 
 const cardContainer = document.querySelector('.gallery')
 
-inputEl.addEventListener('input',onInputSearch)
-
-fetchCard()
-    .then(renderCard)
-    .catch(error => console.log(error))
+searchInputEl.addEventListener('input',onInputSearch)
 
 function onInputSearch(event) {
-    console.log(event.currentTarget.value)
-    const search = event.currentTarget.value
+    const sesrchQuery = event.currentTarget.value
+
+    fetchCard(sesrchQuery)
+    .then(renderCard)
+    .catch(onError)
+    .finally()
 }
 
-function fetchCard() {
-        return fetch(`${BASE_URL}&q=dog&page=1&per_page=12&key=8315600-a916a243d8ea2edafddc43bfd`)
+
+function fetchCard(search) {
+        return fetch(`${BASE_URL}&q=${search}&page=1&per_page=12&key=8315600-a916a243d8ea2edafddc43bfd`)
         .then(res => {return res.json()})
 }
+
 
 function renderCard(cards) {
     const markup = templateCards(cards.hits)
     cardContainer.innerHTML = markup
 }
 
+function onError() {
+    alert('Упс . Щось пійшло не так !')
+}
